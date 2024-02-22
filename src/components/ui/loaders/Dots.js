f4a278e1b7447ebaa8d2fe907579b845f2f68181
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-02-22 14:55:39 +0530

    feat : added component

---
+++ src/components/ui/loaders/Dots.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Dots = () => {
+  return (
+    <div class="loader ">
+        
+    <div class="circle">
+      <div class="dot"></div>
+      <div class="outline"></div>
+    </div>
+    <div class="circle">
+      <div class="dot"></div>
+      <div class="outline"></div>
+    </div>
+    <div class="circle">
+      <div class="dot"></div>
+      <div class="outline"></div>
+    </div>
+    <div class="circle">
+      <div class="dot"></div>
+      <div class="outline"></div>
+    </div>
+  </div>
+  )
+}
+
+export default Dots
